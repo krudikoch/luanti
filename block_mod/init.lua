9f46529fa78e6c413f028e59d2f87260d752620d
Kevin Koch <ruke@Mac-mini-von-Kevin.local>
2025-03-13 20:35:42 +0100

    Edited custom Block it now actually drops/gets added to the player inventory after digging

--- block_mod/init.lua
+++ block_mod/init.lua
@@ -2,16 +2,21 @@ core.register_node("block_mod:wood", {
 	description = "Wood from a Tree",
 	tiles = { "block_mod_woodtop.png", "block_mod_woodtop.png", "block_mod_wood.png" },
 	groups = { choppy = 3, oddly_breakable_by_hand = 2 },
+	on_dig = function(pos, node, digger)
+		local name = digger:get_player_name()
+		if name == "Paul" then
+			minetest.remove_node(pos)
 
-    on_dig = function (pos, node, digger)
-        local name = digger:get_player_name()
-        if name == "Paul" then
-            minetest.remove_node(pos)
-            return true
-        end
+			local inv = digger:get_inventory()
+			if inv:room_for_item("main", "block_mod:wood") then
+				inv:add_item("main", "block_mod:wood")
+			else
+				-- Drop as item if inventory is full
+				minetest.add_item(pos, "block_mod:wood")
+			end
+			return true
+		end
 
-        return false
-    end,
-
-    drops = { items = { items = "block_mod:wood" }},
+		return false
+	end,
 })
