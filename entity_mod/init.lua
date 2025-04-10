8127fa2b4940cd68af963198c7b213689cd1b24b
paul <mail@paul-kleineberg.de>
2025-04-10 14:06:41 +0200

    wip(entity_mod): added sort of path finding to player not fully implement or working completely

--- entity_mod/init.lua
+++ entity_mod/init.lua
@@ -12,30 +12,73 @@ local entity = {
         initial_sprite_basepos = {x = 0, y = 0},
     },
 
-    message = "I got hit",
+    -- in blocks
+    aggro_range = 30,
+
+    fov = 60,
+    raycasts = 15,
+    y_offset = 0.3,
+
+    speed = 1
 }
+
+local function yaw_to_dir(yaw)
+    return {
+        x = -math.sin(yaw),
+        y = 0,
+        z = math.cos(yaw)
+    }
+end
+
+local function find_player_by_raycast(pos, direction, range)
+    local ray = minetest.raycast(pos, vector.add(pos, vector.multiply(direction, range)), true, false)
+    for pointed_thing in ray do
+        if pointed_thing.type == "object" then
+            local obj = pointed_thing.ref
+            if obj:is_player() then
+                return obj, pointed_thing.intersection_point
+            end
+        end
+    end
+    return nil, nil
+end
+
+local function find_player_around_entity(pos, rotation, fov, raycasts, y_offset, aggro_range)
+    local half_fov = fov / 2
+    local yaw_start = rotation.y - half_fov
+    local yaw_end = rotation.y + half_fov + 0.1
+    local raycast_steps = fov / raycasts
+
+    for yaw = yaw_start, yaw_end, raycast_steps do
+        for current_y_offset = -y_offset, y_offset + 0.1, y_offset do
+            local cast_direction = yaw_to_dir(yaw) + vector.new(0, current_y_offset, 0)
+
+            local player, intersection = find_player_by_raycast(pos, cast_direction, aggro_range)
+            if player then
+                return player, vector.distance(pos, intersection)
+            end
+        end
+    end
+end
 
 function entity:on_step(dtime)
     local pos      = self.object:get_pos()
-    local pos_down = vector.subtract(pos, vector.new(0, 1, 0))
+    local rotation = self.object:get_rotation()
 
-    local delta
-    if core.get_node(pos_down).name == "air" then
-        delta = vector.new(0, -1, 0)
-    elseif core.get_node(pos).name == "air" then
-        delta = vector.new(0, 0, 1)
-    else
-        delta = vector.new(0, 1, 0)
+    local player, distance = find_player_around_entity(pos, rotation, self.fov, self.raycasts, self.y_offset, self.aggro_range)
+
+    if player == nil then
+        return;
     end
 
-    delta = vector.multiply(delta, dtime)
+    local player_pos = player:get_pos()
+    local direction = vector.direction(pos, player_pos)
 
-    self.object:move_to(vector.add(pos, delta))
-end
+    self.object:set_rotation(vector.dir_to_rotation(direction))
 
-function entity:on_punch(hitter)
-    core.chat_send_player(hitter:get_player_name(), self.message)
+    local delta = vector.normalize(direction) * dtime * self.speed
+
+    self.object:move_to(pos + delta)
 end
 
 core.register_entity("entity_mod:entity", entity)
-core.add_entity({x = 1, y = 2, z = 3}, "entity_mod:entity", nil)
